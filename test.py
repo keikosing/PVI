27a439f9839b70b8516c57ea4bceb3bfbff4b67a
keikosing <keikosing@gmail.com>
2014-06-05 15:09:21 -0700

    Update test.py

--- test.py
+++ test.py
@@ -21,3 +21,57 @@ with open ('001_shortshort2.csv') as out2:
 			row[1]=eval(row[1])
 			data.append(row)
 #evaluate------------------------------------------
+#make deep copy	
+addC=[]
+for row in data:
+	addC.append(row[:])
+
+#########
+#add extra columns
+########
+for row in addC:
+	row[0:0]=['','','']
+
+############################################
+#move BS/BE and delete
+############################################
+#make deep copy	for movement
+moveBS=[]
+for row in addC:
+	moveBS.append(row[:])
+
+def rearrange(M):
+	initial_length=len(M)
+	N=[]
+	rows_deleted=0
+	breathnumber=0
+	breath_count=0
+	print ('Original: '+str(len(M))+'-->Copied: '+str(len(N)))
+	while M!=[]:
+		if M[0][3]=='BS':
+			breathnumber=breathnumber+1
+			M[1][0]=str(breathnumber)
+			M[1][1]='BS'	
+			#del(M[0])
+			rows_deleted=rows_deleted+1; breath_count=breath_count+1
+		elif M[0][3]=='BE':
+			N[-1][1]='BE'
+			#del(M[0])
+			rows_deleted=rows_deleted+1; breath_count=breath_count+1
+		else:
+			N.append(M[0])
+		del(M[0])
+		#print ('Original: '+str(len(M))+'-->Copy: '+str(len(N)))
+	else:
+		print ('Original: '+str(len(M))+'-->Copied: '+str(len(N))+'\n')
+		print (str(initial_length)+" lines processed from original")
+		print("Rows Deleted: "+str(rows_deleted)+'\n')
+		try:
+			print("Breath Count: "+str(breath_count/2))		
+			print("Breath Count half of deleted rows? "+str(rows_deleted%(breath_count/2)==0))
+		except:
+			print("***Already processed***")
+			pass
+		return N
+
+moveBS=rearrange(moveBS)
